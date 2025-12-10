480d2c4c390df57c20b80c4d7f52d4e3f37ccfff
turisanapo <selvaggiodavide@hotmail.it>
2025-12-10 18:33:24 +0800

    chores(auth): heb-232 Remove Stack hosted UI route and stack react dependency

--- apps/console/app/lib/auth/index.ts
+++ apps/console/app/lib/auth/index.ts
@@ -1,7 +1,13 @@
+import { isAuthEnabled } from "~console/lib/env";
+
+// FUTURE: use dynamic imports to enable tree shaking
 import { authService as betterAuthService } from "./better-auth";
+import { authService as dummyAuthService } from "./dummy-auth";
 
 import type { AuthService } from "./types";
 
-const authService: AuthService = betterAuthService;
+const authService: AuthService = isAuthEnabled
+  ? betterAuthService
+  : (console.warn("⚠️ No auth configured, using dummy"), dummyAuthService);
 
 export { authService };
--- apps/console/app/routes/_auth.handler.$/route.tsx
+++
@@ -1,5 +0,0 @@
-import { Navigate } from "react-router";
-
-export default function AuthHandler() {
-  return <Navigate to="/" replace />;
-}
