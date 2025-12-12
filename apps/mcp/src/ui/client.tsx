4ea50f2d2844a7b275f5578e3b1934069e8bf821
Heinrich Wendel <heinrich.wendel@gmail.com>
2025-12-12 13:40:55 +0800

    chore(mcp): cleanup

--- apps/mcp/src/ui/client.tsx
+++ apps/mcp/src/ui/client.tsx
@@ -2,5 +2,4 @@ import { createRoot } from "react-dom/client";
 
 import { Page } from "./page";
 
-// hydrateRoot(document.getElementById("root")!, <Page />);
 createRoot(document.querySelector("#root")!).render(<Page />);
