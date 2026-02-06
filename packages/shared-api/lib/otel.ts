f4413ce27edb7d897ade004b899722b3bc41c711
turisanapo <selvaggiodavide@hotmail.it>
2026-02-06 15:25:56 +0800

    feat(otel) instrument prisma

--- packages/shared-api/lib/otel.ts
+++ packages/shared-api/lib/otel.ts
@@ -1,6 +1,10 @@
 import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
 import { CompressionAlgorithm } from "@opentelemetry/otlp-exporter-base";
 import { BatchSpanProcessor } from "@opentelemetry/sdk-trace-node";
+import {
+  PrismaInstrumentation,
+  registerInstrumentations,
+} from "@prisma/instrumentation";
 
 import { betterStackConfig } from "./betterstack";
 
@@ -20,6 +24,10 @@ const getTraceExporterConfig = () => {
 };
 
 const traceExporterConfig = getTraceExporterConfig();
+
+registerInstrumentations({
+  instrumentations: [new PrismaInstrumentation()],
+});
 
 export const getOtelConfig = (
   serviceName: string,
